74d7f47e742616874a5b13c94698a3a34a5447bd
mcthoren <thoren.mcdole@gmail.com>
2019-08-14 19:26:41 +0000

    add htu21df_read() to wxlib

--- wxlib.py
+++ wxlib.py
@@ -119,6 +119,51 @@ def pi_temp_read():
 
 	return temp_data
 
+def htu21df_read():
+	# https://www.adafruit.com/product/1899
+	# https://github.com/adafruit/Adafruit_HTU21DF_Library/
+
+	import Adafruit_GPIO.I2C as I2C
+	import math
+
+	temp_read = 0xE3
+	hum_read = 0xE5
+	write_reg = 0xE6
+	soft_reset = 0xFE
+
+	# constants from the datasheet
+	const_a = 8.1332
+	const_b = 1762.39
+	const_c = 235.66
+
+	htu_addy = 0x40
+	bus = 0
+
+	array = [0x00, 0x00]
+
+	dev0 = I2C.Device(htu_addy, bus)
+	dev0.write8(soft_reset, write_reg) # does this actually reset it? prly not....
+	time.sleep(0.2)
+
+	ts =  datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+
+	array = dev0.readList(temp_read, 2)
+	t0 = (array[0] * 256.0) + array[1]
+	temp = ((t0 * 175.72) / 65536) - 46.85
+
+	array = dev0.readList(hum_read, 2)
+	h0 = (array[0] * 256.0) + array[1]
+	hum = ((h0 * 125) / 65536) - 6
+
+	P_part = 10 ** (const_a - (const_b / (temp + const_c)))
+	T_dew = -1 * ((const_b / (math.log10(hum * (P_part / 100)) - const_a)) + const_c)
+
+	dat_string = "%s\tTemp: %.2f C\tHumidity: %.2f %%\tDew Point: %.2f C\n" % (ts, temp, hum, T_dew)
+
+	write_out_dat_stamp(ts, 'htu21df_grab.dat', dat_string)
+
+	return (temp, hum, T_dew)
+
 def bme680_read():
 	# sensor + breakout board from:
 	# https://www.adafruit.com/product/3660

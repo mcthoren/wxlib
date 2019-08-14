534b88aa59eed494d1c2178028101307273becd8
mcthoren <thoren.mcdole@gmail.com>
2019-08-14 18:29:27 +0000

    pull bme680_read() into library

--- wxlib.py
+++ wxlib.py
@@ -119,6 +119,53 @@ def pi_temp_read():
 
 	return temp_data
 
+def bme680_read():
+	# sensor + breakout board from:
+	# https://www.adafruit.com/product/3660
+
+	# libraries and examples from:
+	# https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-bme680-breakout
+
+	import bme680, math, time
+
+	iter = 16
+	avg = 0
+
+	sensor = bme680.BME680(i2c_addr=0x77)
+
+	sensor.set_humidity_oversample(bme680.OS_2X)
+	sensor.set_pressure_oversample(bme680.OS_4X)
+	sensor.set_temperature_oversample(bme680.OS_8X)
+	# sensor.set_filter(bme680.FILTER_SIZE_3)
+	sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+	sensor.set_gas_heater_temperature(320)
+	sensor.set_gas_heater_duration(150)
+	sensor.select_gas_heater_profile(0)
+
+	temp = sensor.data.temperature
+	hum = sensor.data.humidity
+
+	# go back to averaging pressure samples as in the bmp085 datasheet
+	for x in range(0, iter):
+		avg += sensor.data.pressure
+
+	pres_avg = avg / iter # [hPa]
+
+	gas_res = 0
+	tries = 5
+
+	while (tries > 0):
+		sensor.get_sensor_data()
+		if sensor.data.heat_stable:
+			gas_res = sensor.data.gas_resistance
+		else:
+			time.sleep(1)
+
+		tries = tries - 1
+
+	return (temp, hum, pres_avg / 10, gas_res)
+
 def graph(lx, ly, lfmt, ltitle, lylabel, lfname):
 	import matplotlib as mpl
 	mpl.use('Agg')
